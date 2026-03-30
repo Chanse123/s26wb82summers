e74cc3c19811616c0c5ebf7fc6070bb74a1086a9
Chanse123 <s556682@nwmissouri.edu>
2026-03-30 10:31:59 -0500

    add part 6 search results

--- routes/books.js
+++ routes/books.js
@@ -2,7 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('books', { title: 'Search Results: Books' });
+  let results = [
+    { title: 'Dune', author: 'Frank Herbert', price: 18 },
+    { title: '1984', author: 'George Orwell', price: 12 },
+    { title: 'The Hobbit', author: 'J.R.R. Tolkien', price: 15 }
+  ];
+
+  res.render('books', { title: 'Search Results: Books', results: results });
 });
 
 module.exports = router;
